4b5d2ff3fa2c0251a9e89171299a4bfa9a30aa0a
Third Samson <samsonpaulinoedsel@gmail.com>
2025-10-23 12:12:19 +0800

    chore: add Firebase initialization to the client package

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import {router} from "./router/index";
+import "./lib/firebase"; // 👈 initialize firebase here
 
 const app = createApp(App);
 
